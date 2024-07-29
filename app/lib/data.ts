fafce439f2a25c2144e0a8a9d67e59fadd60efeb
Md. Raiyan Takrim <raiyan.takrim3@gmail.com>
2024-07-29 22:20:21 +0600

    Worked on Invoices
    added search feature

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -14,12 +14,7 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-
     const data = await sql<Revenue>`SELECT * FROM revenue`;
-
-    // console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -59,12 +54,19 @@ export async function fetchCardData() {
          SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
          FROM invoices`;
-
+         
+    //⚡⚡⚡remove these 2 lines==============
+    // console.log('Fetching Card data...');
+    // await new Promise((resolve) => setTimeout(resolve, 6000));
+    
     const data = await Promise.all([
       invoiceCountPromise,
       customerCountPromise,
       invoiceStatusPromise,
     ]);
+    
+    //⚡⚡⚡⚡ and this also============================
+    // console.log('Data fetch completed after 6 seconds.');
 
     const numberOfInvoices = Number(data[0].rows[0].count ?? '0');
     const numberOfCustomers = Number(data[1].rows[0].count ?? '0');
